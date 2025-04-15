b92770c7202ed41bd4cb580a233973c7148c6628
Fadi Hassan <ffa.fadi@gmail.com>
2025-04-15 13:58:31 +0300

    Update SpatialExpression.php

--- src/Eloquent/SpatialExpression.php
+++ src/Eloquent/SpatialExpression.php
@@ -3,10 +3,11 @@
 namespace Grimzy\LaravelMysqlSpatial\Eloquent;
 
 use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Grammar;
 
 class SpatialExpression extends Expression
 {
-    public function getValue()
+    public function getValue(Grammar $grammar)
     {
         return "ST_GeomFromText(?, ?, 'axis-order=long-lat')";
     }
